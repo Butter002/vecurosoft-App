9efb499622cf536b48b0a8d7a5f5719e496af9d8
parth <1240sparth@gmail.com>
2025-06-09 16:40:26 +0530

    section4.jsx complete

--- src/HomeSetion/Section4.jsx
+++ src/HomeSetion/Section4.jsx
@@ -4,14 +4,15 @@ import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 import { IoBedSharp } from "react-icons/io5";
 import { BiSolidBuildingHouse } from "react-icons/bi";
-import { FaArrowRight, FaBath, FaKitchenSet } from "react-icons/fa6";
+import { FaBath, FaKitchenSet } from "react-icons/fa6";
 import { ImOffice } from "react-icons/im";
 import { MdOutlineWindow } from "react-icons/md";
 import { GiFloorPolisher, GiVacuumCleaner } from "react-icons/gi";
-import { FaArrowLeft } from 'react-icons/fa';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import { useInView } from "react-intersection-observer";
 
 import "./Section4.scss";
+import "./SliderButtons.css"
 
 import img1 from "../assets/instagram/1.jpg";
 import img2 from "../assets/instagram/2.jpg";
@@ -83,56 +84,58 @@ const cardData = [
           </h2>
           </div>
 
-        {/* card div */}
-        <div className="flex items-center gap-2 flex-row">
-
-        <button className="group w-16 h-16 rounded-full border-4 border-dashed border-gray-300 flex items-center justify-center transition-all duration-300 hover:bg-green-600 hover:border-green-600">
-          <FaArrowLeft className="text-gray-500 group-hover:text-white text-xl transition-colors duration-300" />
-        </button>
-            
-        <div ref={sliderRef} className="keen-slider">
-        {cardData.map((card, index) => (
-          <div className="keen-slider__slide pb-5 " key={index}>
-            <div 
-              className={`relative h-full group rounded-xl m-4 mt-0 pb-5  shadow-md bg-white  lg:m-0  xl:mx-8 transition-all duration-700 ease-out
-              ${inView ? "card-animate opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}
-  
-              style={inView ? { animationDelay: `${index * 0.2}s` } : {}}>
-
-              <div className="overflow-hidden flex flex-col justify-center items-center text-center rounded-t-xl">
-                <img src={card.image} alt={card.title} className="w-full h-50 object-cover transition-transform duration-500 ease-in-out group-hover:scale-110"/>
-              </div>
-
-              <div className="px-6 py-5 flex flex-col  gap-5 items-start">                
-                <div className="flex flex-row border-gray-400  border-b-2 group-hover:border-b-2 group-hover:border-green-600 border-dashed pb-5 items-end w-full justify-between">
-                  <div>
-                    <div className="text-[#109c3d] font-bold text-xl">
-                      CLEANING 0{index + 1}
-                    </div>
-                    <h3 className="font-bold text-2xl text-[#063A41] hover:text-green-600 line-clamp-1 mt-1">
-                      {card.title}
-                    </h3>
-                  </div>
-                  <div className="text-3xl bg-grey">{card.icon}</div>
-                </div>
-
-                <p className="text-gray-500 text-lg">
-                  Aweeps & mopsd vacuum floor House Cleaners.
-                </p>
-              </div>
-              <div className="absolute bottom-0 left-0 w-full h-4 bg-white group-hover:bg-[#109c3d] transition-all duration-300 rounded-b-xl" />
-            </div>
-          </div>
-        ))}
-        </div>
-
-          <button className="group w-16 h-16 rounded-full border-4 border-dashed border-gray-300 flex items-center justify-center transition-all duration-300 hover:bg-green-600 hover:border-green-600">
-            <FaArrowRight className="text-gray-500 group-hover:text-white text-xl transition-colors duration-300" />
+          {/* card sectio  */}
+          <div className="slider-wrapper flex items-center gap-2 flex-row">
+          {/* Left Arrow */}
+          <button onClick={() => instanceRef.current?.prev()} className="slider-arrow hidden lg:flex h-16 w-16 
+          rounded-full border-4 border-dashed border-gray-300 items-center justify-center transition-all duration-300 hover:bg-green-600 hover:border-white">
+          <FaArrowLeft className="arrow-icon text-xl text-gray-500 transition-colors duration-300" />
           </button>
 
-        </div>
+          {/* Slider */}
+          <div ref={sliderRef} className="keen-slider">
+            {cardData.map((card, index) => (
+              <div className="keen-slider__slide pb-5" key={index}>
+                <div
+                  className={`relative h-full group rounded-xl m-4 mt-0 pb-5 shadow-md bg-white lg:m-0 xl:mx-8 
+                  transition-all duration-700 ease-out ${inView ? "card-animate opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}
+                  style={inView ? { animationDelay: `${index * 0.2}s` } : {}} >
+                  <div className="overflow-hidden flex flex-col justify-center items-center text-center rounded-t-xl">
+                    <img
+                      src={card.image}
+                      alt={card.title}
+                      className="w-full h-50 object-cover transition-transform duration-500 ease-in-out group-hover:scale-110"
+                    />
+                  </div>
 
-        
+                  <div className="px-6 py-5 flex flex-col gap-5 items-start">
+                    <div className="flex flex-row border-gray-400 border-b-2 group-hover:border-green-600 border-dashed pb-5 items-end w-full justify-between">
+                      <div>
+                        <div className="text-[#109c3d] font-bold text-xl">
+                          CLEANING 0{index + 1}
+                        </div>
+                        <h3 className="font-bold text-2xl text-[#063A41] hover:text-green-600 line-clamp-1 mt-1">
+                          {card.title}
+                        </h3>
+                      </div>
+                      <div className="text-3xl bg-grey">{card.icon}</div>
+                    </div>
+                    <p className="text-gray-500 text-lg">
+                      Aweeps & mopsd vacuum floor House Cleaners.
+                    </p>
+                  </div>
+                  <div className="absolute bottom-0 left-0 w-full h-4 bg-white group-hover:bg-[#109c3d] transition-all duration-300 rounded-b-xl" />
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {/* Right Arrow */}
+          <button onClick={() => instanceRef.current?.next()} className="slider-arrow hidden lg:flex h-16 w-16 rounded-full
+          border-4 border-dashed border-gray-300 items-center justify-center transition-all duration-300 hover:bg-green-600 hover:border-white" >
+          <FaArrowRight className="arrow-icon text-xl text-gray-400 transition-colors duration-300" />
+          </button>
+          </div>
 
     </div>
   );
